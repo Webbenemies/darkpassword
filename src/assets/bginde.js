04a107e5766f67ed863864f1bb72761689d61c74
SAYAN <sayanhumanist@gmail.com>
2024-03-01 22:28:32 +0530

    opps

--- src/assets/bginde.js
+++ src/assets/bginde.js
@@ -1,6 +1,8 @@
 import bg1 from "./1137.jpg"
 import bg2 from "./leo.jpg"
 import bg3 from "./6117.jpg"
+import bg4 from "./2150793369.jpg"
+import bg5 from "./9126.jpg"
 
-const objimg = [bg1,bg2,bg3]
+const objimg = [bg1,bg2,bg3,bg4,bg5]
 export default objimg
