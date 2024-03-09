2a1aeb83d05607c772675f8ac9197c621a064c76
SAYAN <sayanhumanist@gmail.com>
2024-03-09 15:55:10 +0530

    change bg

--- src/assets/bginde.js
+++ src/assets/bginde.js
@@ -3,6 +3,15 @@ import bg2 from "./leo.jpg"
 import bg3 from "./6117.jpg"
 import bg4 from "./2150793369.jpg"
 import bg5 from "./9126.jpg"
+import bg6 from "./29610.jpg"
+import bg7 from "./30311.jpg"
+import bg8 from "./34610.jpg"
+import bg9 from "./38643.jpg"
+import bg10 from "./39151.jpg"
+import bg11 from "./40981.jpg"
+import bg12 from "./99861.jpg"
+import bg13 from "./2151075935.jpg"
+import bg14 from "./2151151063.jpg"
 
-const objimg = [bg1,bg2,bg3,bg4,bg5]
+const objimg = [bg1,bg2,bg3,bg4,bg5,bg6,bg7,bg8,bg9,bg10,bg11,bg12,bg13,bg14]
 export default objimg
