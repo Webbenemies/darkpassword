c0ad3f414f263b444f79668219f8f738333220ef
SAYAN <sayanhumanist@gmail.com>
2024-03-14 18:48:19 +0530

    .

--- src/assets/bginde.js
+++ src/assets/bginde.js
@@ -15,6 +15,7 @@ import bg14 from "./2151151063.jpg"
 import bg15 from "./5056.jpg"
 import bg16 from "./2151043271.jpg"
 import bg17 from "./2151043301.jpg"
+import bg18 from "./82291.jpg"
 
-const objimg = [bg1,bg2,bg15,bg3,bg4,bg5,bg16,bg6,bg7,bg8,bg9,bg10,bg17,bg11,bg12,bg13,bg14]
+const objimg = [bg1,bg2,bg15,bg18,bg3,bg4,bg5,bg16,bg6,bg7,bg8,bg9,bg10,bg17,bg11,bg12,bg13,bg14]
 export default objimg
