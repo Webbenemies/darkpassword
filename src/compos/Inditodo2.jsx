31bc820d8a700f0c42dd4e470ab76effe7902472
SAYAN <sayanhumanist@gmail.com>
2024-03-24 12:21:44 +0530

    same

--- src/compos/Inditodo2.jsx
+++ src/compos/Inditodo2.jsx
@@ -78,11 +78,11 @@ const Inditodo2 = () => {
             <div className=' w-[100%] h-screen flex items-center justify-center poppins-regular'>
                 <div className=' flex justify-between items-center w-[90%] h-[90vh]  max-sm:w-full max-sm:flex-col-reverse max-sm:gap-4'>
                     <div className=' w-[90%] h-[100%] flex flex-col  max-sm:w-[95%]'>
+                            <p className=' self-end text-[0.6rem] p-1'>{letters} / 50000</p>
                         <div className=' h-full overflow-hidden flex items-center justify-center'>
                             <h1 className="absolute  text-neutral-800 text-[10rem] uppercase  font-semibold z-[-1] opacity-60 max-sm:text-[20vw]">Target.</h1>
                             <textarea style={{scrollbarWidth:"none"}} value={areavalue} readOnly={toggledit} onChange={(e) => setareavalue(e.target.value)} className=' w-[100%] bg-transparent selection:text-amber-500 resize-none border-2 px-2 py-3 outline-none self-stretch max-sm:border-0' spellCheck="false" name="editor"></textarea>
                         </div>
-                            <p className=' self-end text-[0.6rem] p-1 '>{letters} / 50000</p>
                     </div>
 
                     <div className='w-[5%] flex flex-col justify-center items-center gap-4 max-sm:flex-row'>
